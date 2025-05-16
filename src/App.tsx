132fafb7c05437e44c137ca65f339b36e11f7adc
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-05-16 20:30:57 +0530

    feat: add authentication routes to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { Route, Routes, type RouteObject } from "react-router";
 import MainLayout from "./components/Layout";
 import { routes } from "./routes/Routes";
+import AuthLayout from "./features/auth/AuthLayout";
+import { authRoutes } from "./routes/auth.routes";
 
 function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         {routes.map((route: RouteObject) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+      </Route>
+      <Route element={<AuthLayout />}>
+        {authRoutes.map((route: RouteObject) => (
           <Route key={route.path} path={route.path} element={route.element} />
         ))}
       </Route>
