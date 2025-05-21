31efb6476640da95f6ef43059a79676997ca099f
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-05-21 16:48:53 +0530

    feat: enhance PageTitle component with description prop and improved styling

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,16 +1,27 @@
+import { cn } from "@/lib/utils";
 import { BackButton } from "./BackButton";
 
 export function PageTitle({
   title,
+  description,
   withBack,
+  className,
+  ...props
 }: {
   title: string;
+  description?: string;
   withBack?: boolean;
-}) {
+  className?: string;
+} & React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className="flex items-center gap-3">
-      {withBack && <BackButton />}
-      <h2 className="text-3xl font-bold">{title}</h2>
+    <div className={cn("mb-5", className)} {...props}>
+      <div className="flex items-center gap-5">
+        {withBack && <BackButton />}
+        <h2 className="text-2xl font-semibold">{title}</h2>
+      </div>
+      {description && (
+        <p className="mt-2 text-lg leading-4 font-semibold">{description}</p>
+      )}
     </div>
   );
 }
