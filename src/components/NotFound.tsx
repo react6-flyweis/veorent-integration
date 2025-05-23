26c6dd88513e3a9caddc7ff2a98f3dc76d67e4f6
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-05-23 15:41:34 +0530

    refactor: update NotFound component to use custom goBack hook and improve button functionality

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,19 +1,18 @@
-import { useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
+import { useGoBack } from "@/hooks/useGoBack";
 
 export default function NotFound() {
-  const navigate = useNavigate();
-
+  const goBack = useGoBack();
   return (
-    <div className="h-full flex flex-col justify-center items-center text-center">
-      <h1 className="text-6xl font-bold text-primary mb-4">404</h1>
-      <h2 className="text-2xl font-semibold mb-2 text-gray-800">
+    <div className="flex h-full flex-col items-center justify-center text-center">
+      <h1 className="text-primary mb-4 text-6xl font-bold">404</h1>
+      <h2 className="mb-2 text-2xl font-semibold text-gray-800">
         Page Not Found
       </h2>
       <p className="text-muted-foreground mb-6">
         The page you&apos;re looking for doesn&apos;t exist or has been moved.
       </p>
-      <Button onClick={() => navigate("/")}>Go Back Home</Button>
+      <Button onClick={goBack}>Go Back</Button>
     </div>
   );
 }
