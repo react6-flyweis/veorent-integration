d032696b3fb8c67fa86bff448293ea35952851cf
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-05-28 22:51:04 +0530

    feat: update margin and gap styles in PageTitle

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -14,13 +14,13 @@ export function PageTitle({
   className?: string;
 } & React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className={cn("mb-3 @lg:mb-5", className)} {...props}>
-      <div className="flex items-center gap-5">
+    <div className={cn("mb-5", className)} {...props}>
+      <div className="flex items-center gap-3 @lg:gap-5">
         {withBack && <BackButton />}
         <h2 className="text-2xl font-semibold">{title}</h2>
       </div>
       {description && (
-        <p className="mt-2 text-base leading-4 font-semibold text-gray-700">
+        <p className="mt-2 text-sm leading-4 font-semibold text-gray-700 @lg:text-base">
           {description}
         </p>
       )}
