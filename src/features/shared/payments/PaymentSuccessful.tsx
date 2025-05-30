83563c278d3c92067127ea7bb3117b469a46f8e1
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-05-31 00:45:14 +0530

    feat: add check icon for payment success confirmation in PaymentSuccessful component

--- src/features/shared/payments/PaymentSuccessful.tsx
+++ src/features/shared/payments/PaymentSuccessful.tsx
@@ -2,11 +2,20 @@ import { CurrencyIcon } from "@/components/CurrencyIcon";
 import { Button } from "@/components/ui/button";
 import { useGoBack } from "@/hooks/useGoBack";
 
+import checkIcon from "./assets/check.gif";
+
 export default function PaymentSuccessful() {
   const goBack = useGoBack();
   return (
     <div className="flex h-full w-full flex-col items-center justify-center pb-10 @md:justify-end">
       <div className="flex flex-col items-center space-y-4">
+        <img
+          src={checkIcon}
+          alt="Payment Successful"
+          className="border-primary size-28 rounded-full object-cover"
+          // loading="lazy"
+        />
+
         <div className="flex items-center gap-5">
           <CurrencyIcon size="xl" className="text-primary" />
           <h3 className="text-6xl font-bold">514</h3>
