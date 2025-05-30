5c61587709115ca38e136036a0d6ed0b196afd4f
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-05-30 18:37:30 +0530

    feat: add static image in maintenance details

--- src/features/landlord/maintenance/MaintenanceDetails.tsx
+++ src/features/landlord/maintenance/MaintenanceDetails.tsx
@@ -1,6 +1,10 @@
 import { PageTitle } from "@/components/PageTitle";
 import { Card } from "@/components/ui/card";
 
+import brokenPlateImage from "./assets/broken-plate.png";
+import { Link } from "react-router";
+import { Button } from "@/components/ui/button";
+import { ArrowLeftIcon } from "lucide-react";
 interface MaintenanceDetail {
   id: string;
   requestedOn: string;
@@ -18,39 +22,47 @@ export function MaintenanceDetails() {
     category: "Appliances",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    images: ["/broken-plate.jpg"], // Replace with actual image path
+    images: [brokenPlateImage],
   };
 
   return (
     <div className="">
-      <PageTitle title="Details" withBack />
+      <div className="flex gap-5">
+        <Link to="/landlord/maintenance">
+          <Button
+            variant="outline"
+            className="size-9 items-center justify-center rounded-full"
+          >
+            <ArrowLeftIcon className="text-primary size-5" />
+          </Button>
+        </Link>
+        <PageTitle title="Details" />
+      </div>
 
       <Card className="p-6">
         <div className="space-y-4">
-          <div className="grid grid-cols-1 gap-2">
+          <div className="grid grid-cols-1 gap-1">
             <div>
-              <span className="text-primary text-lg font-medium">
+              <span className="text-primary text-lg font-bold">
                 Requested On:
               </span>
               <span className="ml-1">{maintenanceDetail.requestedOn}</span>
             </div>
 
             <div>
-              <span className="text-primary text-lg font-medium">
+              <span className="text-primary text-lg font-bold">
                 Preferred Time to Enter:
               </span>
               <span className="ml-1">{maintenanceDetail.preferredTime}</span>
             </div>
 
             <div>
-              <span className="text-primary text-lg font-medium">
-                Category:
-              </span>
+              <span className="text-primary text-lg font-bold">Category:</span>
               <span className="ml-1">{maintenanceDetail.category}</span>
             </div>
 
             <div>
-              <span className="text-primary text-lg font-medium">
+              <span className="text-primary text-lg font-bold">
                 Description:
               </span>
               <p className="mt-1 text-gray-700">
