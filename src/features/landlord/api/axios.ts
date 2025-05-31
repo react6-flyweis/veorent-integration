f443c7ae0e0daf9dd50e17f7ff46d7909727e315
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-05-31 23:15:26 +0530

    feat: remove token validation check in axiosLandlord configuration

--- src/features/landlord/api/axios.ts
+++ src/features/landlord/api/axios.ts
@@ -3,9 +3,6 @@ import { API_URL } from "@/constants/api";
 import { useAuthStore } from "@/store/useAuthStore";
 
 const token = useAuthStore.getState().token;
-if (!token) {
-  throw new Error("No authentication token found");
-}
 export const axiosLandlord = axiosClint.create({
   baseURL: API_URL + "/partner",
   headers: {
