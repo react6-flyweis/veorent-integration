b2c0d9f4d32052def153ee44890fbd3d4172062c
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-05-31 15:25:40 +0530

    feat: integrate react-query for data fetching

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./queryClient.tsx";
 
 scan({
   enabled: true,
@@ -11,8 +13,11 @@ scan({
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 );
