b6d7ab307c2aacb0475967fb749a244f9308439c
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-06-04 16:55:37 +0530

    feat(landlord): integrate leads fetching and loading state in RentersLeads component

--- src/features/landlord/renters/RentersLeads.tsx
+++ src/features/landlord/renters/RentersLeads.tsx
@@ -7,21 +7,24 @@ import { AddLeadDialog } from "./components/AddLeadDialog";
 
 import { Link } from "react-router-dom";
 import RentersTablist from "./components/RentersTablist";
+import { useGetLeadsQuery } from "./api/queries";
 
 // Sample renter data - in a real app, this would come from a database or API
-const leads = [
-  { name: "Wendy Thompson", address: "46 Bryant Way", invited: true },
-  { name: "Aaron Remirez", address: "36 Bryant Way", invited: false },
-  {
-    name: "Sara Capra",
-    address: "123 Main St.",
-    invited: false,
-    selected: true,
-  },
-];
+// const leads = [
+//   { name: "Wendy Thompson", address: "46 Bryant Way", invited: true },
+//   { name: "Aaron Remirez", address: "36 Bryant Way", invited: false },
+//   {
+//     name: "Sara Capra",
+//     address: "123 Main St.",
+//     invited: false,
+//     selected: true,
+//   },
+// ];
 
 export default function RentersLeads() {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const { data: leads, isLoading } = useGetLeadsQuery();
 
   const handleAddLead = () => {};
 
@@ -38,20 +41,29 @@ export default function RentersLeads() {
       <RentersTablist />
 
       <div className="">
-        {leads.map((renter, index) => (
-          <Link
-            to={"/landlord/renters/leads/" + renter.name.replace(/\s+/g, "-")}
-            key={index}
-          >
-            <RenterItem
-              key={index}
-              name={renter.name}
-              address={renter.address}
-              invited={renter.invited}
-              selected={renter.selected}
-            />
-          </Link>
-        ))}
+        {isLoading ? (
+          <div className="text-center text-gray-500">Loading leads...</div>
+        ) : leads?.length ? (
+          leads
+            .filter((r) => r.propertyInterested) // Filter out renters for dev
+            .map((renter, index) => (
+              <Link to={"/landlord/renters/leads/" + renter._id} key={index}>
+                <RenterItem
+                  key={index}
+                  name={renter.fullName}
+                  address={
+                    renter.propertyInterested?.addressDetails?.streetAddress
+                  }
+                  invited={renter.status === "invited"}
+                  selected={renter.status === "selected"}
+                />
+              </Link>
+            ))
+        ) : (
+          <div className="text-center text-gray-500">
+            No leads found. Click "Add New Lead" to create one.
+          </div>
+        )}
       </div>
 
       <AddLeadDialog
