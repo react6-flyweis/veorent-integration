ce21171cac1adbefe27000c02ae0e2f412ab891d
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-06-04 20:25:41 +0530

    feat: add utility functions for getting initials and full names

---
+++ src/utils/name.ts
@@ -0,0 +1,11 @@
+export const getInitial = (name: string) => {
+  return name
+    .split(" ")
+    .map((part) => part.charAt(0).toUpperCase())
+    .join("")
+    .substring(0, 2);
+};
+
+export const getFullName = (firstName: string, lastName: string) => {
+  return `${firstName.charAt(0).toUpperCase()}${firstName.slice(1)} ${lastName.charAt(0).toUpperCase()}${lastName.slice(1)}`.trim();
+};
