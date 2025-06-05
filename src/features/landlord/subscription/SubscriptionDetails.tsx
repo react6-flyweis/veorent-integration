328be5c50a04fbe41acab48e89b4e6b120a4fd01
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-06-05 17:20:29 +0530

    feat(landlord): integrate SubscriptionDetails to fetch data from API

--- src/features/landlord/subscription/SubscriptionDetails.tsx
+++ src/features/landlord/subscription/SubscriptionDetails.tsx
@@ -5,23 +5,35 @@ import subscriptionDetailsIcon from "./assets/subscription-details.png";
 import { BackButton } from "@/components/BackButton";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { PaymentModeDialog } from "@/features/shared/payments/components/PaymentModeDialog";
+import { Navigate, useParams } from "react-router";
+import { useGetSubscriptionPlanQuery } from "./api/queries";
+
+// const subscriptionPlan = {
+//   title: "Veorent Standard",
+//   price: "15,000",
+//   description:
+//     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+//   period: "Monthly",
+//   features: [
+//     "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+//     "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+//     "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+//     "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+//     "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+//   ],
+// };
 
 export default function SubscriptionDetails() {
-  // This would typically come from an API or store
-  const subscriptionPlan = {
-    title: "Veorent Standard",
-    price: "15,000",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
-    period: "Monthly",
-    features: [
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    ],
-  };
+  const { id } = useParams<{ id: string }>();
+  const { data: subscriptionPlan, isLoading } = useGetSubscriptionPlanQuery(
+    id || "",
+  );
+
+  if (!id) {
+    return <Navigate to="/landlord/subscription" replace />;
+  }
+
+  if (isLoading || !subscriptionPlan) return <></>;
 
   return (
     <div className="container max-w-4xl">
@@ -34,17 +46,23 @@ export default function SubscriptionDetails() {
             alt="Subscription Details"
             className="max-h-9 max-w-9"
           />
-          <h1 className="text-2xl font-bold">{subscriptionPlan.title}</h1>
+          <h1 className="text-2xl font-bold">{subscriptionPlan.name}</h1>
         </div>
 
-        <p className="text-gray-600">{subscriptionPlan.description}</p>
+        {/* <p className="text-gray-600">{subscriptionPlan.description}</p> */}
+        <p className="text-gray-600">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto,
+          quo consequuntur alias culpa voluptatibus accusamus dolore
+          reprehenderit ex autem asperiores debitis magni voluptas nesciunt
+          laudantium quibusdam impedit quis, hic atque.
+        </p>
 
         <div className="rounded-lg border p-2 shadow">
           <div className="flex items-baseline gap-1">
             <CurrencyIcon />
             <span className="text-3xl font-bold">{subscriptionPlan.price}</span>
             <span className="ml-2 text-xl text-gray-500">
-              {subscriptionPlan.period}
+              {subscriptionPlan.billingCycle}
             </span>
           </div>
         </div>
