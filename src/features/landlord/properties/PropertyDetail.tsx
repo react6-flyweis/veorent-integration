2f2a090ac3a258d9a009330ab54250c1e3a4ea4a
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-06-06 21:32:39 +0530

    feat(landlord): refactor PropertyDetail component to use API data and improve status handling

--- src/features/landlord/properties/PropertyDetail.tsx
+++ src/features/landlord/properties/PropertyDetail.tsx
@@ -3,7 +3,6 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { CurrencyIcon } from "@/components/CurrencyIcon";
 import { BackButton } from "@/components/BackButton";
 import {
-  Star,
   UserIcon,
   EditIcon,
   EllipsisVerticalIcon,
@@ -15,107 +14,135 @@ import {
 } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { useState } from "react";
+import { useGetPropertyByIdQuery } from "./api/queries";
+import { Badge } from "@/components/ui/badge";
+import { useMemo } from "react";
+import { cn } from "@/lib/utils";
 
-type PropertyDetailType = {
-  id: string;
-  name: string;
-  address: string;
-  city: string;
-  state: string;
-  zipCode: string;
-  image: string;
-  baths?: number;
-  beds?: number;
-  rent?: number;
-  rating?: number;
-  status?: "complete" | "incomplete" | "marketing" | undefined;
-  type?: "single" | "multi";
-  description?: string;
-  propertyId?: string;
-  marketingExpires?: string;
-  listingUrl?: string;
-  preScreenerUrl?: string;
+// type PropertyDetailType = {
+//   id: string;
+//   name: string;
+//   address: string;
+//   city: string;
+//   state: string;
+//   zipCode: string;
+//   image: string;
+//   baths?: number;
+//   beds?: number;
+//   rent?: number;
+//   rating?: number;
+//   status?: "complete" | "incomplete" | "marketing" | undefined;
+//   type?: "single" | "multi";
+//   description?: string;
+//   propertyId?: string;
+//   marketingExpires?: string;
+//   listingUrl?: string;
+//   preScreenerUrl?: string;
+// };
+
+// const properties: PropertyDetailType[] = [
+//   {
+//     id: "1",
+//     name: "Ft. Collins Home",
+//     address: "700 H St",
+//     city: "Ft Collins",
+//     state: "CO",
+//     zipCode: "80992",
+//     image: "/property.png",
+//     baths: 2,
+//     beds: 3,
+//     rent: 900,
+//     rating: 5,
+//     status: "marketing",
+//     type: "single",
+//     description:
+//       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+//     propertyId: "742148",
+//     marketingExpires: "08/17/2024 (29 DAYS LEFT)",
+//     listingUrl: "https://example/examplewebsite.com",
+//     preScreenerUrl: "https://example/examplewebsite.com",
+//   },
+//   {
+//     id: "2",
+//     name: "46 Bryant Way",
+//     address: "46 Bryant Way",
+//     city: "Denver",
+//     state: "CO",
+//     zipCode: "80219",
+//     image: "/property.png",
+//     baths: 2,
+//     beds: 2,
+//     rent: 4000,
+//     status: undefined,
+//     type: "single",
+//     description:
+//       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+//     propertyId: "749289",
+//     marketingExpires: "08/17/2024 (29 DAYS LEFT)",
+//     listingUrl: "https://example/examplewebsite.com",
+//     preScreenerUrl: "https://example/examplewebsite.com",
+//   },
+//   {
+//     id: "3",
+//     name: "Family House in Santa Barbara",
+//     address: "2100 Circle Drive",
+//     city: "Santa Barbara",
+//     state: "CA",
+//     zipCode: "87692",
+//     image: "/property.png",
+//     baths: 1,
+//     beds: 2,
+//     rent: 1500,
+//     status: "incomplete",
+//     type: "single",
+//     description:
+//       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+//     propertyId: "742151",
+//     marketingExpires: "08/17/2024 (29 DAYS LEFT)",
+//     listingUrl: "https://example/examplewebsite.com",
+//     preScreenerUrl: "https://example/examplewebsite.com",
+//   },
+// ];
+
+const statusColorMap: Record<string, string> = {
+  marketing: "bg-blue-500",
+  incomplete: "bg-orange-500",
 };
-
-const properties: PropertyDetailType[] = [
-  {
-    id: "1",
-    name: "Ft. Collins Home",
-    address: "700 H St",
-    city: "Ft Collins",
-    state: "CO",
-    zipCode: "80992",
-    image: "/property.png",
-    baths: 2,
-    beds: 3,
-    rent: 900,
-    rating: 5,
-    status: "marketing",
-    type: "single",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-    propertyId: "742148",
-    marketingExpires: "08/17/2024 (29 DAYS LEFT)",
-    listingUrl: "https://example/examplewebsite.com",
-    preScreenerUrl: "https://example/examplewebsite.com",
-  },
-  {
-    id: "2",
-    name: "46 Bryant Way",
-    address: "46 Bryant Way",
-    city: "Denver",
-    state: "CO",
-    zipCode: "80219",
-    image: "/property.png",
-    baths: 2,
-    beds: 2,
-    rent: 4000,
-    status: undefined,
-    type: "single",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-    propertyId: "749289",
-    marketingExpires: "08/17/2024 (29 DAYS LEFT)",
-    listingUrl: "https://example/examplewebsite.com",
-    preScreenerUrl: "https://example/examplewebsite.com",
-  },
-  {
-    id: "3",
-    name: "Family House in Santa Barbara",
-    address: "2100 Circle Drive",
-    city: "Santa Barbara",
-    state: "CA",
-    zipCode: "87692",
-    image: "/property.png",
-    baths: 1,
-    beds: 2,
-    rent: 1500,
-    status: "incomplete",
-    type: "single",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-    propertyId: "742151",
-    marketingExpires: "08/17/2024 (29 DAYS LEFT)",
-    listingUrl: "https://example/examplewebsite.com",
-    preScreenerUrl: "https://example/examplewebsite.com",
-  },
-];
-
 export default function PropertyDetail() {
   const { id: propertyId } = useParams();
   const navigate = useNavigate();
+  const { data } = useGetPropertyByIdQuery(propertyId || "");
 
-  const [extended, setExtended] = useState(false);
+  if (!data) {
+    return <div>Loading...</div>;
+  }
 
-  const property = properties.find((p) => p.id === propertyId) || properties[0];
+  const property = data;
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const isIncomplete = useMemo(() => {
+    return (
+      property.formCompletionStatus &&
+      !Object.values(property.formCompletionStatus).every(
+        (status) => status === true,
+      )
+    );
+  }, [property.formCompletionStatus]);
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const status = useMemo(() => {
+    // logic for status marketing
+    if (property.formCompletionStatus)
+      return isIncomplete ? "incomplete" : "complete";
+    return property.status;
+  }, [property, isIncomplete]);
 
   return (
     <div className="flex flex-col gap-4 pb-8">
       <Card className="gap-0 rounded-sm p-0">
         {/* Property Hero Section */}
-        <CardHeader className="px-0">
-          <div className="relative">
+        <CardHeader className="bg-primary h-64 items-center px-0 py-0">
+          <div className="relative flex h-full items-center justify-center">
             <div className="absolute top-2 left-2 z-10">
               <BackButton />
             </div>
@@ -139,23 +166,22 @@ export default function PropertyDetail() {
               </Button>
             </div>
 
-            {/* {property.status === "marketing" && (
-          <Badge className="absolute top-2 left-2 z-10 bg-blue-500 text-white">
-            Marketing
-          </Badge>
-        )}
+            <Badge
+              className={cn(
+                "absolute top-2 left-2 z-10 text-white",
+                statusColorMap[status] || "bg-gray-500",
+              )}
+            >
+              {status.charAt(0).toUpperCase() + status.slice(1)}
+            </Badge>
 
-        {property.status === "incomplete" && (
-          <Badge className="absolute top-2 left-2 z-10 bg-orange-500 text-white">
-            Incomplete
-          </Badge>
-        )} */}
-
-            <img
-              src={property.image}
-              alt={property.name}
-              className="h-64 w-full object-cover"
-            />
+            <div className="size-32 rounded-full bg-gray-100">
+              <img
+                src={property.image[0]?.img}
+                alt={property.name}
+                className="h-full w-full object-cover"
+              />
+            </div>
           </div>
         </CardHeader>
         {/* Property Info Section */}
@@ -163,8 +189,10 @@ export default function PropertyDetail() {
           <div className="flex flex-col gap-4">
             <div>
               <div className="flex gap-2">
-                <h2 className="text-xl font-semibold">{property.address}</h2>
-                {property.rating && (
+                <h2 className="text-xl font-semibold">
+                  {property.addressDetails?.streetAddress}
+                </h2>
+                {/* {property.rating && (
                   <div className="mt-1 flex">
                     {Array.from({ length: property.rating }).map((_, i) => (
                       <Star
@@ -173,10 +201,12 @@ export default function PropertyDetail() {
                       />
                     ))}
                   </div>
-                )}
+                )} */}
               </div>
               <p className="text-muted-foreground">
-                {property.city}, {property.state} {property.zipCode}
+                {property.addressDetails?.city},{" "}
+                {property.addressDetails?.region}{" "}
+                {property.addressDetails?.zipCode}
               </p>
             </div>
 
@@ -185,14 +215,14 @@ export default function PropertyDetail() {
                 <span className="text-muted-foreground text-sm">
                   Property ID
                 </span>
-                <p className="font-medium">{property.propertyId}</p>
+                <p className="font-medium">{property._id}</p>
               </div>
 
               <div className="flex items-center gap-2">
                 <span className="text-muted-foreground text-sm">Rent</span>
-                <p className="flex items-center font-medium">
+                <p className="flex items-center gap-1 font-medium">
                   <CurrencyIcon size="sm" />
-                  {property.rent?.toLocaleString()}.00
+                  {property.rentalDetails?.targetRent?.toLocaleString()}
                 </p>
               </div>
             </div>
@@ -212,32 +242,28 @@ export default function PropertyDetail() {
 
       {/* Marketing Status Section */}
       <Card className="rounded-sm py-0">
-        {property.status === "marketing" && (
+        {status === "marketing" && (
           <div className="rounded-md p-4">
             <h3 className="mb-2 text-lg font-medium">Marketing Online: ON</h3>
             <p className="text-muted-foreground text-sm">
-              EXPIRES ON {property.marketingExpires}
+              <span>EXPIRES ON</span>
+              {/* <span>{property.marketingExpires}</span> */}
             </p>
             <div className="mt-4 flex justify-around gap-5">
               <Button className="flex-1" variant="outlinePrimary">
                 Turn Off
               </Button>
-
-              {!extended && (
-                <Link
-                  to="/landlord/properties/marketing-extended"
-                  className="flex-1"
-                >
-                  <Button onClick={() => setExtended(true)} className="w-full">
-                    Extend
-                  </Button>
-                </Link>
-              )}
+              <Link
+                to="/landlord/properties/marketing-extended"
+                className="flex-1"
+              >
+                <Button className="w-full">Extend</Button>
+              </Link>
             </div>
           </div>
         )}
 
-        {property.status === undefined && (
+        {status === "complete" && (
           <div className="rounded-md p-4">
             <h3 className="mb-2 text-2xl font-bold">
               Market My Rental For Free
@@ -274,7 +300,7 @@ export default function PropertyDetail() {
           </div>
         )}
 
-        {property.status === "incomplete" && (
+        {status === "incomplete" && (
           <div className="p-4">
             <h3 className="mb-2 text-lg font-medium">Marketing Online: OFF</h3>
             <p className="text-muted-foreground text-sm">
@@ -286,23 +312,24 @@ export default function PropertyDetail() {
       </Card>
 
       {/* Action Buttons */}
-      {property.status === "incomplete" ||
-        (!property.status && (
-          <div className="flex justify-center">
-            <Link className="w-4/5" to="/landlord/properties/setup">
-              <Button size="lg" className="mt-4 w-full">
-                {property.status ? "Complete" : "Create"} My Listing
-              </Button>
-            </Link>
-          </div>
-        ))}
-      {property.status === "marketing" && (
+      {(status === "incomplete" || !status) && (
+        <div className="flex justify-center">
+          <Link
+            className="w-4/5"
+            to={`/landlord/properties/${property._id}/setup`}
+          >
+            <Button size="lg" className="mt-4 w-full">
+              {status ? "Complete" : "Create"} My Listing
+            </Button>
+          </Link>
+        </div>
+      )}
+      {status === "marketing" && (
         <Card className="py-4">
           <CardContent className="space-y-5 px-4">
             <div className="space-y-1">
               <h3 className="text-lg font-medium">Listing Page</h3>
-
-              <p className="text-blue-600">{property.listingUrl}</p>
+              <p className="text-blue-600">{property._id}</p>
               <div className="flex gap-5">
                 <Button
                   size="sm"
@@ -336,7 +363,7 @@ export default function PropertyDetail() {
                 <BadgeInfoIcon className="size-4" />
               </div>
 
-              <p className="text-blue-600">{property.listingUrl}</p>
+              <p className="text-blue-600">{property._id}</p>
               <div className="flex gap-5">
                 <Button
                   size="sm"
@@ -359,7 +386,7 @@ export default function PropertyDetail() {
       )}
 
       {/* Footer action buttons for non-marketing properties */}
-      {/* {property.status !== "marketing" && (
+      {/* {status !== "marketing" && (
         <Card>
           <CardFooter className="grid grid-cols-2 gap-4 p-4">
             <Button variant="outline" className="w-full">
