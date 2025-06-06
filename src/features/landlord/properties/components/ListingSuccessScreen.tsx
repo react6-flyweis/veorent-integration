337ef82a17b29e9482fe48d4569cbf4a0b412cd5
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-06-06 22:22:50 +0530

    feat(landlord): add listing image and update ListingSuccessScreen layout

--- src/features/landlord/properties/components/ListingSuccessScreen.tsx
+++ src/features/landlord/properties/components/ListingSuccessScreen.tsx
@@ -1,13 +1,16 @@
+import { IconRound } from "@/components/IconRound";
 import { Button } from "@/components/ui/button";
-import { HouseIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
+import setupListingImage from "../assets/setup-listing.gif";
+import listingImage from "../assets/listing.png";
+
 interface ListingSuccessScreenProps {
-  address?: string;
+  propertyName?: string;
 }
 
 export const ListingSuccessScreen = ({
-  address = "123 Main St.",
+  propertyName,
 }: ListingSuccessScreenProps) => {
   const navigate = useNavigate();
 
@@ -17,17 +20,24 @@ export const ListingSuccessScreen = ({
 
   return (
     <div className="flex flex-col justify-between">
-      <div className="mb-2 flex items-center">
-        <div className="rounded-full bg-blue-100 p-2">
-          <HouseIcon className="h-6 w-6 text-blue-600" />
-        </div>
-
-        <h2 className="text-2xl font-bold text-gray-900">
+      <div className="mb-2 flex items-center gap-3">
+        <IconRound icon={listingImage} size="sm" />
+        <h2 className="text-2xl font-bold">
           Your Listing Is Successfully Published
         </h2>
       </div>
-      <p className="text-gray-600">{address}</p>
-      <div className="min-h-52 flex-1"></div>
+
+      {propertyName && (
+        <div className="text-primary mb-5 text-xl">{propertyName}</div>
+      )}
+
+      <div className="flex justify-center">
+        <img
+          src={setupListingImage}
+          alt="Setup Listing"
+          className="w-full max-w-md rounded-lg shadow-lg"
+        />
+      </div>
       <div className="flex items-center justify-center">
         <Button className="w-4/5 @lg:w-3/5" onClick={handleBackClick}>
           Back
