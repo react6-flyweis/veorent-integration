e06c5213e49aaa8c86b03930ef5f17e65c59ca3c
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-06-10 15:21:53 +0530

    fix: rename queryclient file from tsx to ts and fix linting error.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import { scan } from "react-scan"; // must be imported before React and React
 import { StrictMode } from "react";
+import { scan } from "react-scan"; // must be imported before React and React
 import { createRoot } from "react-dom/client";
+
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./queryClient.tsx";
+
+import { queryClient } from "./queryClient";
 
 scan({
   enabled: true,
