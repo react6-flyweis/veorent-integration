3ba7cc8c75817a090464ee879ba3394fc81cb887
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-06-13 13:53:50 +0530

    feat(auth): restore user type redirection logic in AuthWrapper component

--- src/features/core/auth/AuthWrapper.tsx
+++ src/features/core/auth/AuthWrapper.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
+
 import { useAuthStore } from "@/store/useAuthStore";
 
 interface AuthWrapperProps {
@@ -13,6 +14,7 @@ export const AuthWrapper = ({
 }: AuthWrapperProps) => {
   // In a real app, you would check for authentication status as well
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+  const user = useAuthStore((state) => state.user);
   // const user = useAuthStore((state) => state.user);
 
   // If we're requiring authentication and the user isn't authenticated, redirect to login
@@ -21,14 +23,19 @@ export const AuthWrapper = ({
   }
 
   // Redirect to their preferred section if they're trying to access the wrong area
-  // if (user?.userType ) {
-  //   return (
-  //     <Navigate
-  //       to={`/${user.userType === "USER" ? "tenant" : "landlord"}`}
-  //       replace
-  //     />
-  //   );
-  // }
+  if (
+    (user?.userType === "USER" &&
+      window.location.pathname.startsWith("/landlord")) ||
+    (user?.userType === "PARTNER" &&
+      window.location.pathname.startsWith("/tenant"))
+  ) {
+    return (
+      <Navigate
+        to={`/${user.userType === "USER" ? "tenant" : "landlord"}`}
+        replace
+      />
+    );
+  }
 
   // If we don't know their preference yet, go to personalize
   // return <Navigate to="/choose" replace />;
