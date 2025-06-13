043d83a63620cae4b6ded73837f2e2bd1c43f06f
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-06-13 13:53:08 +0530

    feat(tenant): refactor useGetProfileQuery to update user state in useEffect

--- src/features/tenant/settings/api/queries.ts
+++ src/features/tenant/settings/api/queries.ts
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 
 import { useAuthStore } from "@/store/useAuthStore";
@@ -7,16 +8,20 @@ import { axiosTenant } from "../../api/axios";
 export const useGetProfileQuery = () => {
   const updateUser = useAuthStore((state) => state.updateUser);
 
-  return useQuery({
+  const query = useQuery({
     queryFn: () =>
       axiosTenant.get<IResponse<{ memberSince: string; user: IUser }>>(
         "/profile",
       ),
     queryKey: ["user", "profile"],
-    select: (data) => {
-      const user = data.data.data.user;
-      updateUser(user);
-      return user;
-    },
+    select: (data) => data.data.data.user,
   });
+
+  useEffect(() => {
+    if (query.data) {
+      updateUser(query.data);
+    }
+  }, [query.data, updateUser]);
+
+  return query;
 };
