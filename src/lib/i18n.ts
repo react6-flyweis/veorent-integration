79baa9d97fe234d425733e3b5264cc04a49a8023
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-04 20:54:43 +0530

    feat(landlord): add landlord localization for English, Spanish, and French

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -3,10 +3,13 @@ import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
 import en from "@/locales/en/common.json";
+import enLandlord from "@/locales/en/landlord.json";
 import enTenant from "@/locales/en/tenant.json";
 import es from "@/locales/es/common.json";
+import esLandlord from "@/locales/es/landlord.json";
 import esTenant from "@/locales/es/tenant.json";
 import fr from "@/locales/fr/common.json";
+import frLandlord from "@/locales/fr/landlord.json";
 import frTenant from "@/locales/fr/tenant.json";
 
 i18n
@@ -15,13 +18,13 @@ i18n
   .init({
     resources: {
       en: {
-        translation: Object.assign({}, en, enTenant),
+        translation: Object.assign({}, en, enTenant, enLandlord),
       },
       es: {
-        translation: Object.assign({}, es, esTenant),
+        translation: Object.assign({}, es, esTenant, esLandlord),
       },
       fr: {
-        translation: Object.assign({}, fr, frTenant),
+        translation: Object.assign({}, fr, frTenant, frLandlord),
       },
     },
     fallbackLng: "en",
